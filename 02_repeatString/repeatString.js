848c47791fea345aac35c77247d002ebc4d5352b
Richard Dominguez <richard.domi90@gmail.com>
2022-12-21 11:07:41 -0800

    mod: repeatstring test pass
    code for repeatString passes all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function (word = '', num = 0) {
+    let i = 1;
 
+    if(word == ''){
+        return '';
+    }
+    if (num == 0) {
+        return '';
+    } 
+    else if (num > 0) {
+        word = 'hey'
+        for (num; i < num; i++) {
+            word = word +'hey'
+        };
+        return word
+    } 
+    else {
+        return 'ERROR';
+    };
 };
+
+// console.log(repeatString('hey',10));
 
 // Do not edit below this line
 module.exports = repeatString;
