aa5e4f4bce5cbc62f69663e951fd29f70f50a613
Richard Dominguez <richard.domi90@gmail.com>
2022-12-21 13:48:03 -0800

    mod: removeFromArray test pass
    Add passing program that passes test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (a = [], ...args) {
+    args_length = args.length;
+    let i = 0;
+    // let new_array = [];
+    let item_rm = 0;
 
+    for (i; i < args_length; i++) {
+        if (a.includes(args[i])) {
+            item_rm = a.indexOf(args[i]);
+            a.splice(item_rm, 1);
+        };
+    };
+    return a;
 };
+
+// console.log(removeFromArray([1, 2, 3], 12, 3, 4, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
