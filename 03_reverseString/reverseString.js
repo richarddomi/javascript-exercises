7f88770873615192f934a4dd5f6c062528433be0
Richard Dominguez <richard.domi90@gmail.com>
2022-12-21 12:00:38 -0800

    mod: reverseString test pass
    reverseString program passes all test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+clearImmediate;
 
+const reverseString = function (phrase = '') {
+    let length = phrase.length;
+    let i = length-1; // shift down to initiate count from 0
+    let new_word = '';
+
+    // i +1 is required because it will not account for the first letter which sits at 0
+    while (i+1) {
+        new_word = new_word + phrase[i]; 
+        i--;
+    }
+    return new_word;
 };
+
+// console.log(reverseString("abcdefghijklmno"));
 
 // Do not edit below this line
 module.exports = reverseString;
