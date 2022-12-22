6e12304121e1cb3091b452d88d98a1699179d036
Richard Dominguez <richard.domi90@gmail.com>
2022-12-21 17:56:30 -0800

    mod: sumAll test pass
    Add Program for sumALL that passes all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+let upper = 0; lower = 0;
 
+const sumAll = function (x = 0, y = 0) {
+    let j = 0;
+
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+
+    if (x > y) {
+        lower = y;
+        upper = x;
+    }
+    else if (x < y) {
+        lower = x;
+        upper = y;
+    }
+    else if (x == y) {
+        return 0;
+    };
+
+    if (lower >= 0 && upper >= 0) {
+        let i = lower;
+        for (i; i <= upper; i++) {
+            j += i;
+        };
+        return j;
+    }
+    else {
+        return 'ERROR';
+    };
 };
-
+// console.log(sumAll(-1, 4));
 // Do not edit below this line
 module.exports = sumAll;
